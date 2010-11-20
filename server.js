21fc43980431e723c8d616adc7f2a715c5287996
Tylor Sherman <tylorsherman@gmail.com>
2010-11-20 00:54:14 -0800

    Add mongodb support for nutella

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var sys         = require('sys')
   , TwitterNode = require('twitter-node').TwitterNode
   ;
 
+// Get MongoDB ready.
+var Db          = require('mongodb').Db
+  , Connection  = require('mongodb').Connection
+  , Server      = require('mongodb').Server
+  , BSON        = require('mongodb').BSONNative
+  ;
+
+var host        = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+var port        = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
+
+var db          = new Db('bieber', new Server(host, port, {}), {native_parser:true});
+
 // Homepage.
 app.get('/', function(req, res){
   res.render('index.ejs');
@@ -28,22 +40,28 @@ socket.on('connection', function(client){
   // Dragons.
 });
 
-// Listen for new tweets and broadcast to socket.
-twit
-  .addListener('tweet', function(tweet) {
-    // Send out new Tweets to everyone connected.
-    socket.broadcast(tweet);
-  })
-  .stream(); // And start streaming.
+db.open(function(err, db) {
+  db.collection('tweets', function(err, collection) {
+    // Listen for new tweets and broadcast to socket.
+    twit
+      .addListener('tweet', function(tweet) {
+        // Add to mongodb.
+        collection.insert(tweet);
+        // Send out new Tweets to everyone connected.
+        socket.broadcast(tweet);
+      })
+      .stream(); // And start streaming.
 
-// Set up some Twitter stream debugging niceties.
-twit
-  .addListener('end', function(resp) {
-    console.log("Twitter waves goodbye... " + resp.statusCode);
-    if (resp.statusCode == 401) {
-      console.log("You probably haven't entered your Twitter credentials properly.");
-    }
-  })
-  .addListener('error', function(error) {
-    console.log('Twit error: ' + error.message);
+    // Set up some Twitter stream debugging niceties.
+    twit
+      .addListener('end', function(resp) {
+        console.log("Twitter waves goodbye... " + resp.statusCode);
+        if (resp.statusCode == 401) {
+          console.log("You probably haven't entered your Twitter credentials properly.");
+        }
+      })
+      .addListener('error', function(error) {
+        console.log('Twit error: ' + error.message);
+      });
   });
+});
